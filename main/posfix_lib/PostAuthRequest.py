37afb9c0a54e1b895751f61c25bed247da833fcc
Mahmut Celik <mahmut.celik@pozitiftech.com>
2024-11-07 02:43:40 +0300

    PostAuth

---
+++ main/posfix_lib/PostAuthRequest.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+import json
+from main.posfix_lib.Helper import Helper, HttpClient
+from xml.etree.ElementTree import Element, SubElement, tostring
+
+
+class PostAuthRequest(object):
+    OrderId = ""
+    Amount = ""
+    ClientIp = ""
+    Mode = ""
+
+    # 3D Secure Olmadan Odeme Servis cagsirini temsil eder.
+    def execute(self, req, configs):
+        helper = Helper()
+        configs.TransactionDate = helper.GetTransactionDateString()
+
+        configs.HashString = configs.PrivateKey + req.OrderId + req.Amount + req.Mode + \
+                             req.ClientIp+ configs.TransactionDate
+
+        json_data = json.dumps({
+            'orderId': req.OrderId,
+            'amount': req.Amount,
+            'mode': req.Mode,
+            'clientIp': req.ClientIp
+        })
+
+        result = HttpClient.post(configs.BaseUrl + "rest/payment/postauth",
+                                 helper.GetHttpHeaders(
+                                     configs, helper.Application_json),
+                                 json_data)
+
+        return result
